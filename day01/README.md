f45996ef9bc8cf6955045caf10e33e6007aa9a3f
Jack Light <jacklight@outlook.com>
2025-12-22 22:28:09 -0500

    Day 1: band name generator

---
+++ day01/README.md
@@ -0,0 +1,7 @@
+Day 1 - Python basics
+
+Practiced:
+1. Print Statements
+2. Output statements
+3. Variable types
+4. Concat 
---
+++ day01/main.py
@@ -0,0 +1,13 @@
+#Band name generator
+
+print("Welcome to the band name generator.")
+
+#capture input for hometown
+city = input("What is the name of the city you grew up in?:\n")
+
+#capture user input for pet name
+pet = input("What's your pet's name?:\n")
+
+
+#display band name as concat of city and pet name 
+print(f"Your band name could be {city} {pet}")
